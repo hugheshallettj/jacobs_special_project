f9130624255fed297daf07dffb5b43d62a124877
Jacob Hughes-Hallett <hh.jacob@gmail.com>
2024-05-30 10:25:05 +0100

    Added the survey models: survey, question and response.

--- event_surveys_project/surveys/models.py
+++ event_surveys_project/surveys/models.py
@@ -1,3 +1,66 @@
 from django.db import models
+from django.urls import reverse
+from django.utils.translation import ugettext_lazy as _ # this doesnt change the functionality of the code
+from django.conf import settings # this imports the settings so that we can use our custom auth user model
 
 # Create your models here.
+class Survey(models.Model):
+
+    admin = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    link = models.URLField(unique=True)
+
+    class Meta:
+        verbose_name = _("Survey") # the underscore is a translation key for django
+        verbose_name_plural = _("Surveys")
+
+    def __str__(self):
+        return f"{self.name}-{self.start_date}"
+
+    def get_absolute_url(self):
+        return reverse("Survey_detail", kwargs={"pk": self.pk})
+    
+class Question(models.Model):
+    
+    QUESTION_TYPES = (
+        ('MS', 'Multi Select'),
+        ('SS','Single Select'),
+        ('STX','Short Text'),
+        ('LTX','Long Text'),
+        ('DAT','Date'),
+        ('INT', 'Integer'),
+    )
+
+    survey = models.ForeignKey(Survey, related_name='questions', on_delete=models.CASCADE)
+    question_text = models.CharField(max_length=255)
+    question_type = models.CharField(max_length=3, choices=QUESTION_TYPES)
+
+    class Meta:
+        verbose_name = _("Question")
+        verbose_name_plural = _("Questions")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("Question_detail", kwargs={"pk": self.pk})
+    
+class Response(models.Model):
+
+    survey = models.ForeignKey(Survey, related_name='responses', on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, related_name='responses', on_delete=models.CASCADE)
+    answer = models.TextField()
+
+    class Meta:
+        verbose_name = _("Response")
+        verbose_name_plural = _("Responses")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("Response_detail", kwargs={"pk": self.pk})
+
+
