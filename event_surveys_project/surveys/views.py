da7c76c053582a15d6194c373bf148c3b89cea67
Jacob Hughes-Hallett <hh.jacob@gmail.com>
2024-05-30 16:19:00 +0100

    Added the view for create_survey

--- event_surveys_project/surveys/views.py
+++ event_surveys_project/surveys/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Survey, Question, Response
+from datetime import datetime
 
-# Create your views here.
+@login_required
+def create_survey(request):
+    if request.method == 'POST':
+        # Handle survey creation form submission
+        pass
+    return render(request, 'surveys/create_survey.html')
+
+# def survey_view(request, link):
+#     survey = get_object_or_404(Survey, link=link)
+#     if survey.start_date > datetime.today() or survey.end_date < datetime.today():
+#         return render(request, 'surveys/survey_unavailable.html')
+#     if request.method == 'POST':
+#         # Handle survey response submission
+#         pass
+#     return render(request, 'surveys/survey.html', {'survey': survey})
+
+# @login_required
+# def survey_results(request, survey_id):
+#     survey = get_object_or_404(Survey, id=survey_id)
+#     responses = Response.objects.filter(survey=survey)
+#     return render(request, 'surveys/results.html', {'survey': survey, 'responses': responses})
