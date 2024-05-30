d7e325210818cc5cd0bdcd0199ca22848604163b
Jacob Hughes-Hallett <hh.jacob@gmail.com>
2024-05-30 12:05:51 +0100

    Linked the views with the templates, so that the home page should now display something

--- event_surveys_project/event_surveys_project/urls.py
+++ event_surveys_project/event_surveys_project/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', home, name='home')
+    path('', views.home, name='home')
 ]
