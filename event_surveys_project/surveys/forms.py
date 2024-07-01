875b864c20c9b6179c1ce94a3ee77fd255b29175
Jacob Hughes-Hallett <hh.jacob@gmail.com>
2024-07-01 15:59:54 +0100

    Automatically generate survey form from survey model data

--- event_surveys_project/surveys/forms.py
+++ event_surveys_project/surveys/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Survey, Question
+from .models import Survey, Question, Response
 
 
 class SurveyCreationForm(forms.ModelForm):
@@ -27,3 +27,32 @@ class QuestionForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['select_options'].required = False
+
+class SurveyResponseForm(forms.ModelForm):
+    class Meta:
+        model = Response
+        fields = ['answer']
+
+def generate_survey_form(survey):
+    class SurveyForm(forms.Form):
+        def __init__(self, *args, **kwargs):
+            super(SurveyForm, self).__init__(*args, **kwargs)
+            for question in survey.questions.all():
+                if question.question_type == 'MS' or question.question_type == 'SS':
+                    choices = [(opt, opt) for opt in question.select_options]
+                    if question.question_type == 'MS':
+                        self.fields[f'question_{question.id}'] = forms.MultipleChoiceField(
+                            choices=choices, widget=forms.CheckboxSelectMultiple, label=question.question_text)
+                    else:
+                        self.fields[f'question_{question.id}'] = forms.ChoiceField(
+                            choices=choices, widget=forms.RadioSelect, label=question.question_text)
+                elif question.question_type == 'STX':
+                    self.fields[f'question_{question.id}'] = forms.CharField(label=question.question_text, widget=forms.TextInput)
+                elif question.question_type == 'LTX':
+                    self.fields[f'question_{question.id}'] = forms.CharField(label=question.question_text, widget=forms.Textarea)
+                elif question.question_type == 'DAT':
+                    self.fields[f'question_{question.id}'] = forms.DateField(label=question.question_text, widget=forms.DateInput(attrs={'type': 'date'}))
+                elif question.question_type == 'INT':
+                    self.fields[f'question_{question.id}'] = forms.IntegerField(label=question.question_text)
+    return SurveyForm
+
